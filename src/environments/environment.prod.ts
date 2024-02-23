364a3abb98610e5bda4e91a20456997efb7d8792
SVLucasAndres <lucassangurima2@gmail.com>
2024-02-23 07:48:54 -0500

    con capacitor

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAa1aS62o1GLsGI9EJ8V-F2X9ajIKt-M7Q",
+    authDomain: "practica-1-9a0ef.firebaseapp.com",
+    databaseURL: "https://practica-1-9a0ef-default-rtdb.firebaseio.com",
+    projectId: "practica-1-9a0ef",
+    storageBucket: "practica-1-9a0ef.appspot.com",
+    messagingSenderId: "240476820986",
+    appId: "1:240476820986:web:4699cd61923de9f1c6c043"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAa1aS62o1GLsGI9EJ8V-F2X9ajIKt-M7Q",
+    authDomain: "practica-1-9a0ef.firebaseapp.com",
+    databaseURL: "https://practica-1-9a0ef-default-rtdb.firebaseio.com",
+    projectId: "practica-1-9a0ef",
+    storageBucket: "practica-1-9a0ef.appspot.com",
+    messagingSenderId: "240476820986",
+    appId: "1:240476820986:web:4699cd61923de9f1c6c043"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
